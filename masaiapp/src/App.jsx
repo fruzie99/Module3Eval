693831ebdc159f8cfe3b957ea034dbfe34c7b57b
fruzie99 <vmadhurisha99@gmail.com>
2026-01-03 21:07:21 +0530

    errors fixed

--- masaiapp/src/App.jsx
+++ masaiapp/src/App.jsx
@@ -1,34 +1,49 @@
-import {Routes,Route} from "react-router-dom";
-import { BrowserRouter
- } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import CustomerDashboard from "./pages/CustomerDashboard";
-import ProtectedRoute from "./routes/ProtectedRoute";
 import UpdateRestaurant from "./pages/UpdateRestaurant";
- 
 
-export default function App(){
-  // console.log("hello");
-  return(
-    
-    
-    <BrowserRouter>
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+export default function App() {
+  return (
     <Routes>
+     
       <Route path="/" element={<Login />} />
-      <Route path="/admin/dashboard" element={<ProtectedRoute role="admin"><AdminDashboard /></ProtectedRoute>} />
 
+  
+      <Route
+        path="/admin/dashboard"
+        element={
+          <ProtectedRoute role="admin">
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
 
+      <Route
+        path="/update/:id"
+        element={
+          <ProtectedRoute role="admin">
+            <UpdateRestaurant />
+          </ProtectedRoute>
+        }
+      />
 
-      <Route path="/customer/dashboard" element={<ProtectedRoute role="customer"><CustomerDashboard /></ProtectedRoute>} />
+      
+      <Route
+        path="/customer/dashboard"
+        element={
+          <ProtectedRoute role="customer">
+            <CustomerDashboard />
+          </ProtectedRoute>
+        }
+      />
 
-
-
-      <Route path="/update/:id" element={<ProtectedRoute role="admin"><UpdateRestaurant /></ProtectedRoute>} />
-
-
-
+      
+      <Route path="*" element={<h2>404 Page Not Found</h2>} />
     </Routes>
-    </BrowserRouter>
-  )
+  );
 }
